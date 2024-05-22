3e18430e60f63f49cbf4ac869e5da41812fd1caf
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-05-22 21:06:06 +0300

    Loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -=1
+        print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return[x**2 for x in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+
+
